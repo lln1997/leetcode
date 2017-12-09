d74afe1255e0637200ddc3b36d0db45b30fec5a1
lln1997 <1908626143@qq.com>
2017-12-09 19:21:23 +0800

    Update Convert Sorted List to Binary Search Tree.java

--- LinkedList/Convert Sorted List to Binary Search Tree.java
+++ LinkedList/Convert Sorted List to Binary Search Tree.java
@@ -15,14 +15,24 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {  
+class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         if(head==null) return null;
         return toBST(head,null);
     }
     
     public TreeNode toBST(ListNode head,ListNode tail){
-        TreeNode tn = null;
-        
+        ListNode slow = head,fast = head;
+        if(head==tail){
+            return null;
+        }
+        while(fast!=tail&&fast.next!=tail){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode f = new TreeNode(slow.val);
+        f.left = toBST(head,slow);
+        f.right = toBST(slow.next,tail);
+        return f;
     }
 }
